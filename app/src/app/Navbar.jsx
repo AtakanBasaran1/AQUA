273597bf0433a390bf01ad9f3ff20eb48d8b0d9b
Atakan <158854874+Atakanlift@users.noreply.github.com>
2024-09-27 04:17:30 +0300

    dwa
    dwa

--- app/src/app/Navbar.jsx
+++ app/src/app/Navbar.jsx
@@ -6,8 +6,8 @@ import { IoPerson } from "react-icons/io5";
 
 const Navbar = () => {
     return (
-        <div className='fixed bottom-0 text-center w-full z-50 bg-custom-blue rounded-t-[50px] h-[50px] text-[16px] p-4'>
-            <div className='flex text-center justify-center gap-5 mt-[-3px]'>
+        <div className='fixed bottom-0 text-center w-full z-50 bg-custom-blue rounded-t-[50px] h-[60px] text-[16px] p-4'>
+            <div className='flex text-center justify-center gap-5 mt-[3px]'>
                 <GoHomeFill onClick={() => window.location.href = '/'} className='text-[26px] cursor-pointer text-gray-500 hover:text-white mt-[1px] ' />
                 <TbStackBack className='text-[29px] mt-[-1px] cursor-pointer text-gray-500 hover:text-white ' />
                 <FiBookmark className='text-[24px] cursor-pointer text-gray-500 hover:text-white mt-[1px] ' />
